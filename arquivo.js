453b9a6212da4326f4908ea632fd115256885d0c
Felipe Stamboni <123281130+felpzsccp@users.noreply.github.com>
2023-03-28 17:46:40 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,89 @@
+// PROMPT
+
+var num1 = prompt('Digite o valor de num1')
+var num2 = prompt('Digite o valor de num1')
+
+
+num1 = parseInt(num1)
+num2 = parseInt(num2)
+
+// ADIÇÃO
+
+
+
+document.write('A soma entre '+ num1 +' e '+ num2 +' é: ' + (num1 + num2)+ '<br/>')
+
+//-----------------------------------------------------------------------
+
+
+
+// SUBTRAÇÃO
+
+
+document.write('A subtração entre '+ num1 +' e '+ num2 +' é: ' + (num1 - num2)+ 
+'<br/>')
+
+
+//--------------------------------------------------------------------------
+
+
+
+// MULTIPLICAÇÃO
+
+
+document.write('A multiplicação entre '+ num1 +' e '+ num2 +' é: ' + (num1 * num2)+ '<br/>')
+
+
+//--------------------------------------------------------------------------
+
+
+
+// DIVISÂO
+
+
+document.write('A divisão entre '+ num1 +' e '+ num2 +' é: ' + (num1 / num2)+ '<br/>')
+
+
+//--------------------------------------------------------------------------
+
+
+
+// MÓDULO
+
+
+document.write('O módulo entre '+ num1 +' e '+ num2 +' é: ' + (num1 % num2)+ '<br/>')
+
+
+//--------------------------------------------------------------------------
+
+
+
+// INCREMENTO
+
+document.write('O incremento de  '+ num1 +' é: ' + (++num1)+ '<br/>')
+
+
+
+// INCREMENTO POS
+//document.write('O incremento de '+ num1 +' é: ' + (num1++)+ '<br/>')
+//document.write(num1)
+
+// O RESULTADO DO INCREMENTO IRÁ FICAR DE 9:9
+
+
+//-------------------------------------------------------------------------
+
+
+// DECREMENTO
+
+
+document.write('O decremento de '+ num1 +' é: ' + (--num1)+ '<br/>')
+
+// DECREMENTO POS
+
+
+
+//document.write('O decremento de '+ num1 +' é: ' + (num1--)+ '<br/>')
+//document.write(num1)
+
+// O RESULTADO DO DECREMENTO IRÁ FICAR DE 10:10
